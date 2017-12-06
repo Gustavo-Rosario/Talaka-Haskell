fc03619e5060c94ff89d24a46f01594de3a153b3
Talaka <guqueroz@gmail.com>
2017-12-06 23:28:18 +0000

    Talaka - Menu com validacao

--- src/Application.hs
+++ src/Application.hs
@@ -48,6 +48,7 @@ import Handler.View
 import Handler.Login
 import Handler.Utils
 import Handler.Search
+import Handler.Common
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
