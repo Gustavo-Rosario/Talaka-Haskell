b529c83ee3a77d93ae3ab4a730d5dafc49516df8
Talaka <guqueroz@gmail.com>
2017-12-06 15:57:44 +0000

    Talaka - Alteracao da home | Correção de Ver Perfil

--- src/Handler/View.hs
+++ src/Handler/View.hs
@@ -10,14 +10,26 @@ import Import
 import Text.Cassius
 import Database.Persist.Postgresql
 import Handler.Utils
+import qualified Prelude as P
 
 getHomeR :: Handler Html
 getHomeR = do
     logged <- isLogged
-    defaultLayout $ do
-        setTitle "Talaka Pocket - Plataforma de Financiamento para Quadrinhos"
-        addStylesheet $ StaticR css_bootstrap_css
-        case logged of
-            Just 1 -> $(whamletFile "templates/indexUser.hamlet")
-            Just 2 -> $(whamletFile "templates/indexAdmin.hamlet")
-            Nothing -> $(whamletFile "templates/index.hamlet")
+    case logged of
+        Just 1 -> do
+            (Just user) <- lookupSession "_USERID"
+            Just (Entity userId _) <- runDB $ selectFirst [UserId ==. ( P.read . unpack $ user) ] []
+            defaultLayout $ do
+                setTitle "Talaka Pocket - Plataforma de Financiamento para Quadrinhos"
+                addStylesheet $ StaticR css_bootstrap_css
+                $(whamletFile "templates/indexUser.hamlet")
+        Just 2 -> do
+            defaultLayout $ do
+                setTitle "Talaka Pocket - Plataforma de Financiamento para Quadrinhos"
+                addStylesheet $ StaticR css_bootstrap_css
+                $(whamletFile "templates/indexAdmin.hamlet")
+        Nothing -> do
+            defaultLayout $ do
+                setTitle "Talaka Pocket - Plataforma de Financiamento para Quadrinhos"
+                addStylesheet $ StaticR css_bootstrap_css
+                $(whamletFile "templates/index.hamlet")
